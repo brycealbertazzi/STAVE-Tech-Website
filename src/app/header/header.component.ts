ad791169b7114964be779949bef40d09a7f4b192
brycealbertazzi <yourgitemail@example.com>
2021-02-16 13:33:17 -0800

    Improvements to UI and Mobile appearance

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  constructor(private route: Router) { }
 
-  constructor() { }
+  ngOnInit() {
+  }
 
-  ngOnInit(): void {
+  // Returns the route of the page we're on
+  getRoute() {
+    return this.route.url;
   }
 
 }
